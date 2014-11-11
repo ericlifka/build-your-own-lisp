1d3aeca1198dc2b87f5431654bba77ef27f7ca4a
Eric Lifka <eric.lifka@gmail.com>
2014-11-11 00:00:45 -0500

    switch to s expressions and 4 spaces

--- parsing.c
+++ parsing.c
@@ -113,43 +113,44 @@ lval eval(mpc_ast_t* t) {
 }
 
 int main(int argc, char** argv) {
-  /* Create Some Parsers */
-  mpc_parser_t* Number   = mpc_new("number");
-  mpc_parser_t* Operator = mpc_new("operator");
-  mpc_parser_t* Expr     = mpc_new("expr");
-  mpc_parser_t* Lispy    = mpc_new("lispy");
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Symbol = mpc_new("symbol");
+    mpc_parser_t* Sexpr  = mpc_new("sexpr");
+    mpc_parser_t* Expr   = mpc_new("expr");
+    mpc_parser_t* Lispy  = mpc_new("lispy");
 
-  /* Define them with the following Language */
-  mpca_lang(MPCA_LANG_DEFAULT,
-    "                                                     \
-      number   : /-?[0-9]+/ ;                             \
-      operator : '+' | '-' | '*' | '/' ;                  \
-      expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-      lispy    : /^/ <operator> <expr>+ /$/ ;             \
-    ",
-    Number, Operator, Expr, Lispy);
+    mpca_lang(MPCA_LANG_DEFAULT,
+      "                                          \
+        number : /-?[0-9]+/ ;                    \
+        symbol : '+' | '-' | '*' | '/' ;         \
+        sexpr  : '(' <expr>* ')' ;               \
+        expr   : <number> | <symbol> | <sexpr> ; \
+        lispy  : /^/ <expr>* /$/ ;               \
+      ",
+      Number, Symbol, Sexpr, Expr, Lispy);
 
-  puts("Lispy Version 0.0.0.0.1");
-  puts("Press Ctr+c to Exit\n");
+    puts("Lispy Version 0.0.0.0.1");
+    puts("Press Ctr+c to Exit\n");
 
-  while (1) {
-  	char* input = readline("lispy> ");
-  	add_history(input);
+    while (1) {
+        char* input = readline("lispy> ");
+        add_history(input);
 
-    mpc_result_t r;
-    if (mpc_parse("<stdin>", input, Lispy, &r)) {
-      lval result = eval(r.output);
-      lval_println(result);
-      mpc_ast_delete(r.output);
+        mpc_result_t r;
+        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+            lval result = eval(r.output);
+            lval_println(result);
+            mpc_ast_delete(r.output);
+        }
+        else {
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
+
+        free(input);
     }
-    else {
-      mpc_err_print(r.error);
-      mpc_err_delete(r.error);
-    }
 
-  	free(input);
-  }
+    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
 
-  mpc_cleanup(4, Number, Operator, Expr, Lispy);
-  return 0;
+    return 0;
 }
