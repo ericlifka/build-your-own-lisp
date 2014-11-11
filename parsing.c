7131dd7806157097a2943a1fb2c4b0031e0d10b6
Eric Lifka <eric.lifka@gmail.com>
2014-11-11 00:45:48 -0500

    cleanup

--- parsing.c
+++ parsing.c
@@ -99,19 +99,6 @@ void lval_println(lval v) {
     putchar('\n');
 }
 
-int number_of_nodes(mpc_ast_t* t) {
-  if (t->children_num == 0) {
-    return 1;
-  }
-  else {
-    int total = 1;
-    for (int i = 0; i < t->children_num; i++) {
-      total = total + number_of_nodes(t->children[i]);
-    }
-    return total;
-  }
-}
-
 lval* lval_read_num(mpc_ast_t* t) {
     errno = 0;
     long x = strtol(t->contents, NULL, 10);
