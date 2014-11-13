89bd8a327e26a833668cba4580a726aefc7d78ef
Eric Lifka <eric.lifka@gmail.com>
2014-11-12 23:47:14 -0500

    fix warnings and errors by defining my struct properly

--- parsing.c
+++ parsing.c
@@ -4,7 +4,7 @@
 #include "mpc.h"
 
 /* lval's represent result values */
-typedef struct {
+typedef struct lval {
     int type;
     long num;
     char* err;
@@ -283,7 +283,7 @@ int main(int argc, char** argv) {
 
         mpc_result_t r;
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            lval* x = lval_read(r.output);
+            lval* x = lval_eval(lval_read(r.output));
             lval_println(x);
             lval_del(x);
             mpc_ast_delete(r.output);
