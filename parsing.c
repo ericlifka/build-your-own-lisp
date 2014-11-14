4c441a2b3ae59c3826cdfc1162510c6e01717f40
Eric Lifka <eric.lifka@gmail.com>
2014-11-13 19:48:07 -0500

    add grammar for q-expressions

--- parsing.c
+++ parsing.c
@@ -261,6 +261,7 @@ int main(int argc, char** argv) {
     mpc_parser_t* Number = mpc_new("number");
     mpc_parser_t* Symbol = mpc_new("symbol");
     mpc_parser_t* Sexpr  = mpc_new("sexpr");
+    mpc_parser_t* Qexpr  = mpc_new("qexpr");
     mpc_parser_t* Expr   = mpc_new("expr");
     mpc_parser_t* Lispy  = mpc_new("lispy");
 
@@ -269,10 +270,11 @@ int main(int argc, char** argv) {
         number : /-?[0-9]+/ ;                    \
         symbol : '+' | '-' | '*' | '/' ;         \
         sexpr  : '(' <expr>* ')' ;               \
+        qexpr  : '{' <expr>* '}' ;               \
         expr   : <number> | <symbol> | <sexpr> ; \
         lispy  : /^/ <expr>* /$/ ;               \
       ",
-      Number, Symbol, Sexpr, Expr, Lispy);
+      Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
 
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctr+c to Exit\n");
@@ -296,7 +298,7 @@ int main(int argc, char** argv) {
         free(input);
     }
 
-    mpc_cleanup(5, Number, Symbol, Sexpr, Expr, Lispy);
+    mpc_cleanup(6, Number, Symbol, Sexpr, Qexpr, Expr, Lispy);
 
     return 0;
 }
