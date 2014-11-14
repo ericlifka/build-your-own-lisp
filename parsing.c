d011e49d206fa8e9750774f066640abc8ef0fd12
Eric Lifka <eric.lifka@gmail.com>
2014-11-13 21:25:51 -0500

    first pass at head and tail functions

--- parsing.c
+++ parsing.c
@@ -222,6 +222,51 @@ lval* builtin_op(lval* a, char* op) {
     return x;
 }
 
+lval* builtin_head(lval* a) {
+    if (a->count != 1) {
+        lval_del(a);
+        return lval_err("Function 'head' accepts exactly one argument");
+    }
+
+    if (a->cell[0]->type != LVAL_QEXPR) {
+        lval_del(a);
+        return lval_err("Function 'head' passed incorrect types!");
+    }
+
+    if (a->cell[0]->count == 0) {
+        lval_del(a);
+        return lval_err("Function 'head' passed {}!");
+    }
+
+    lval* v = lval_take(a, 0);
+    while (v->count > 1) {
+        lval_del(lval_pop(v, 1));
+    }
+
+    return v;
+}
+
+lval* builtin_tail(lval* a) {
+    if (a->count != 1) {
+        lval_del(a);
+        return lval_err("Function 'tail' passed too many arguments!");
+    }
+
+    if (a->cell[0]->type != LVAL_QEXPR) {
+        lval_del(a);
+        return lval_err("Function 'tail' passed incorrect types!");
+    }
+
+    if (a->cell[0]->count == 0) {
+        lval_del(a);
+        return lval_err("Function 'tail' passed {}!");
+    }
+
+    lval* v = lval_take(a, 0);
+    lval_del(lval_pop(v, 0));
+    return v;
+}
+
 lval* lval_eval(lval* v);
 lval* lval_eval_sexpr(lval* v) {
     /* Evaluate all children first so that we can act on a
