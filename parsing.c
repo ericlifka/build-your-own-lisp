f9c603a7ff165e37c668c2000852f3796353efbd
Eric Lifka <eric.lifka@gmail.com>
2014-11-24 12:37:15 -0500

    define builtin function to create lambdas

--- parsing.c
+++ parsing.c
@@ -540,6 +540,23 @@ lval* builtin(lenv* e, lval* a, char* func) {
     return lval_err("Unknown Function '%s'", func);
 }
 
+lval* builtin_lambda(lenv* e, lval* a) {
+    LASSERT_NUM("\\", a, 2);
+    LASSERT_TYPE("\\", a, 0, LVAL_QEXPR);
+    LASSERT_TYPE("\\", a, 1, LVAL_QEXPR);
+    for (int i = 0; i < a->cell[0]->count; i++) {
+        LASSERT(a, (a->cell[0]->cell[i]->type == LVAL_SYM),
+            "Cannot define non-symbol. Got %s, Expected %s",
+            ltype_name(a->cell[0]->cell[i]->type), ltype_name(LVAL_SYM));
+    }
+
+    lval* formals = lval_pop(a, 0);
+    lval* body = lval_pop(a, 0);
+    lval_del(a);
+
+    return lval_lambda(formals, body);
+}
+
 lval* lval_eval_sexpr(lenv* e, lval* v) {
     /* Evaluate all children first so that we can act on a
        flat list of values
