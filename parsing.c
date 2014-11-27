fb8257080fe00cf1572c95a39b43ca58f8ca3562
Eric Lifka <eric.lifka@gmail.com>
2014-11-27 00:36:14 -0500

    comparison function

--- parsing.c
+++ parsing.c
@@ -647,6 +647,44 @@ lval* builtin_ord(lenv* e, lval* a, char* op) {
     return lval_num(r);
 }
 
+int lval_eq(lval* x, lval* y) {
+    if (x->type != y->type) {
+        return 0;
+    }
+
+    switch(x->type) {
+
+    case LVAL_NUM: return (x->num == y->num);
+
+    case LVAL_ERR: return (strcmp(x->err, y->err) == 0);
+    case LVAL_SYM: return (strcmp(x->sym, y->sym) == 0);
+
+    case LVAL_FUN:
+        if (x->builtin || y->builtin) {
+            return x->builtin == y->builtin;
+        } else {
+            return lval_eq(x->formals, y->formals) &&
+                lval_eq(x->body, y->body);
+        }
+
+    case LVAL_QEXPR:
+    case LVAL_SEXPR:
+        if (x->count != y->count) {
+            return 0;
+        }
+
+        for (int i = 0; i < x->count; i++) {
+            if (!lval_eq(x->cell[i], y->cell[i])) {
+                return 0;
+            }
+        }
+        return 1;
+
+    }
+
+    return 0;
+}
+
 lval* builtin_gt(lenv* e, lval* a) {
     return builtin_ord(e, a, ">");
 }
