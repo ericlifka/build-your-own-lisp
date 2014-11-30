5f3ce8c5b18791a02d3524b261d2478da340744b
Eric Lifka <eric.lifka@gmail.com>
2014-11-30 12:28:31 -0500

    add loading of files specified as command line args

--- parsing.c
+++ parsing.c
@@ -1019,31 +1019,45 @@ int main(int argc, char** argv) {
       ",
       Number, Symbol, String, Comment, Sexpr, Qexpr, Expr, Lispy);
 
-    puts("Lispy Version 0.0.0.0.1");
-    puts("Press Ctr+c to Exit\n");
-
     lenv* e = lenv_new();
     lenv_add_builtins(e);
 
-    while (1) {
-        char* input = readline("lispy> ");
-        add_history(input);
+    if (argc == 1) { // No files given so run interactive prompt
+        puts("Lispy Version 0.0.0.0.1");
+        puts("Press Ctr+c to Exit\n");
 
-        mpc_result_t r;
-        if (mpc_parse("<stdin>", input, Lispy, &r)) {
+        while (1) {
+            char* input = readline("lispy> ");
+            add_history(input);
 
-            lval* x = lval_eval(e, lval_read(r.output));
-            lval_println(x);
+            mpc_result_t r;
+            if (mpc_parse("<stdin>", input, Lispy, &r)) {
+
+                lval* x = lval_eval(e, lval_read(r.output));
+                lval_println(x);
+                lval_del(x);
+
+                mpc_ast_delete(r.output);
+            }
+            else {
+                mpc_err_print(r.error);
+                mpc_err_delete(r.error);
+            }
+
+            free(input);
+        }
+
+    }
+
+    if (argc >= 2) { // load and run the specified files in order
+        for (int i = 1; i < argc; i++) {
+            lval* args = lval_add(lval_sexpr(), lval_str(argv[i]));
+            lval* x = builtin_load(e, args);
+            if (x->type == LVAL_ERR) {
+                lval_println(x);
+            }
             lval_del(x);
-
-            mpc_ast_delete(r.output);
         }
-        else {
-            mpc_err_print(r.error);
-            mpc_err_delete(r.error);
-        }
-
-        free(input);
     }
 
     lenv_del(e);
